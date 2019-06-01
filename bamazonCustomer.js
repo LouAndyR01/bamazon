1ff0b5bf827e0ffd05999afd23f52ba12f728f50
Louis Rodriguez <louisp285@yahoo.com>
2019-06-01 13:52:15 -0500

    not working - need to fix

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -9,12 +10,36 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected  as id " + connection.threadId);
-    showInventory();
+connection.connect();
+
+var display = function () {
+   connection.query("SELECT * FROM products", function(err,res) { 
+       if (err) throw err;
+       console.log ("---------------------------");
+       console.log ("     Welcome to Bamazon   ");
+       console.log ("---------------------------");
+       console.log ("");
+       console.log ("Find Your Product Below");
+       console.log ("");
+      
+   }); 
+   var table = new Table({
+    head: ['item_id', 'product_name', 'department_name', "price", "stock_quantity"],
+    colWidths: [12, 50, 8],
+    colAligns: ['center', 'left', 'right'],
+    style: {
+        head: ["aqua"],
+        compact: true
+    }
 });
 
-var displayProducts = function () {
+for (var i = 0; i < res.length; i++) {
+    table.push([res[i].id, res[i].products_name, res[i].department_name,  res[i].price, res[i].stock_quantity]);
+}
+console.log(table.toString());
+console.log("");
 
 };
+display();
+
+
