5fd53f7e0cb6f558b8aca5fe221be1ac7d1516db
Louis Rodriguez <louisp285@yahoo.com>
2019-06-02 13:46:00 -0500

    fixed connection and populating products, finally

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,23 +1,41 @@
+//mysql node package
 var mysql = require("mysql");
+
+//inquirer package 
 var inquirer = require("inquirer");
 
+
+
+//create connection with the server and database
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "password",
-    database: "bamazon_db"
+
+//password and DB
+    password: "smu2019",
+    database: "bamazon_DB",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    queryAllProducts();
+    // console.log("hello");
+
+    showProducts();
 });
 
-function queryAllProducts() {
-    connection.query("SELECT * FROM products", function (err, res) {
+function showProducts(){
+    connection.query("SELECT * FROM products", function (err, res){
         if (err) throw err;
         console.log(res);
+        initialize()
     });
-}
+};
+
+/////---------------------------------------------/////
+
+
+
+
+  
+
