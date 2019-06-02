613c781c09229ab3eb95c6b0ec13790f46bcec0f
Louis Rodriguez <louisp285@yahoo.com>
2019-06-02 12:00:03 -0500

    redo to fix problem

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,36 +9,15 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect();
-
-var display = function () {
-   connection.query("SELECT * FROM products", function(err,res) { 
-       if (err) throw err;
-       console.log ("---------------------------");
-       console.log ("     Welcome to Bamazon   ");
-       console.log ("---------------------------");
-       console.log ("");
-       console.log ("Find Your Product Below");
-       console.log ("");
-      
-   }); 
-   var table = new Table({
-    head: ['item_id', 'product_name', 'department_name', "price", "stock_quantity"],
-    colWidths: [12, 50, 8],
-    colAligns: ['center', 'left', 'right'],
-    style: {
-        head: ["aqua"],
-        compact: true
-    }
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    queryAllProducts();
 });
 
-for (var i = 0; i < res.length; i++) {
-    table.push([res[i].id, res[i].products_name, res[i].department_name,  res[i].price, res[i].stock_quantity]);
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+    });
 }
-console.log(table.toString());
-console.log("");
-
-};
-display();
-
-
