1d98b5c24e32206d44fbe7605990c40829b62196
Louis Rodriguez <louisp285@yahoo.com>
2019-06-03 23:41:06 -0500

    added comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,9 @@ var mysql = require("mysql");
 //inquirer package 
 var inquirer = require("inquirer");
 
+//display table from node_modules
 var Table = require("cli-table2");
+
 //create connection with the server and database
 var connection = mysql.createConnection({
     host: "localhost",
@@ -16,12 +18,13 @@ var connection = mysql.createConnection({
     database: "bamazon_DB",
 });
 
+//establish connection with node
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
-
 });
 
+//the table gets set up to display products
 var viewProducts = function(){
     var query = "SELECT * FROM products";
     connection.query(query, function(err, res) {
@@ -29,15 +32,15 @@ var viewProducts = function(){
       var displayTable = new Table ({
         head: ["Item ID", "Product Name", "Category", "Price", "Quantity"],
         colWidths: [15,30,30,15,15]
-      });
+});
       for (var i = 0; i < res.length; i++){
         displayTable.push(
           [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
-        );
-      }
+    );
+        }
       console.log(displayTable.toString());
       buyPrompt();
-    });
+});
   }
   
   function buyPrompt(){
@@ -51,25 +54,25 @@ var viewProducts = function(){
       {
         name: "Quantity",
         type: "input",
-        message: "How many of this product would you like to buy?",
+        message: "How many units of this product would you like to buy?",
         filter: Number
       },
     ]).then(function(input){
       var idInput = input.ID;
       var quantityInput = input.Quantity;
-      placeOrder(idInput, quantityInput);
+      enterOrder(idInput, quantityInput);
     });
   };
   
-  function placeOrder(ID, amtNeeded){
+  function enterOrder(ID, productNeeded){
     connection.query("Select * FROM products WHERE item_id = " + ID, function(err,res){
       if(err){console.log(err)};
-      if(amtNeeded <= res[0].stock_quantity){
-        var totalPrice = res[0].price * amtNeeded;
+      if(productNeeded <= res[0].stock_quantity){
+        var totalPrice = res[0].price * productNeeded;
         console.log("Your order has been completed");
-        console.log("Your total for " + amtNeeded + " " + res[0].product_name + " is " + totalPrice + ".");
+        console.log("Your total for " + productNeeded + " " + res[0].product_name + " is " + totalPrice + ".");
   
-        connection.query("UPDATE products SET stock_quantity =  " + (res[0].stock_quantity - amtNeeded) + " WHERE item_id = " + ID);
+        connection.query("UPDATE products SET stock_quantity =  " + (res[0].stock_quantity - productNeeded) + " WHERE item_id = " + ID);
       } else {
         console.log("Sorry we are out of " + res[0].product_name + "!");
       };
